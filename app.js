01ef1ac068fcf7f0d1385b5ef2a9b15c7641e59a
Tamara Reyda <tamara.reyda@gmail.com>
2014-09-02 23:55:09 -0700

    added jquery source and basic posting functionality

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var main = function() {
-	
+	$('.button').click(function() {
+		var post = $('.status-box').val();
+		$('<li>').text(post).prependTo('.posts');
+		$('.status-box').val('');
+	});
 };
 
 $(document).ready(main);
