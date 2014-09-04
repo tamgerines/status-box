3b3d92c90b134ddc48e10fde3c41517f0145bdb6
Tamara Reyda <tamara.reyda@gmail.com>
2014-09-03 17:02:52 -0700

    character counter works

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var main = function() {
 		var post = $('.status-box').val();
 		$('<li>').text(post).prependTo('.posts');
 		$('.status-box').val('');
+		$('.counter').text('140');
+	});
+	$('.status-box').keyup(function() {
+		var postLength = $(this).val().length;
+		var charactersLeft = 140 - postLength;
+		$('.counter').text(charactersLeft);
 	});
 };
 
