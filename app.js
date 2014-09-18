84fd5fc860af13f1ca09164863e917361232dc48
Tamara Reyda <tamara.reyda@gmail.com>
2014-09-17 20:55:23 -0700

    disabled button when appropriate and updated styling

--- app.js
+++ app.js
@@ -4,12 +4,27 @@ var main = function() {
 		$('<li>').text(post).prependTo('.posts');
 		$('.status-box').val('');
 		$('.counter').text('140');
+		$('.button').prop('disabled', true);
+		$('.button').addClass('disabled');
 	});
 	$('.status-box').keyup(function() {
 		var postLength = $(this).val().length;
 		var charactersLeft = 140 - postLength;
 		$('.counter').text(charactersLeft);
+		
+		if (charactersLeft < 0) {
+			$('.button').prop('disabled', true);
+			$('.button').addClass('disabled');
+		} else if (charactersLeft == 140) {
+			$('.button').prop('disabled', true);
+			$('.button').addClass('disabled');
+		} else {
+			$('.button').prop('disabled', false);
+			$('.button').removeClass('disabled');
+		}
 	});
+	$('.button').prop('disabled', true);
+	$('.button').addClass('disabled');
 };
 
 $(document).ready(main);
